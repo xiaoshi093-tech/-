4c0f08df73ca7e6c5fe5909759d6c8cd7d2db38a
水利转码 <xiaoshi093@gmail.com>
2026-04-08 18:04:43 +0800

    关于for的学习

---
+++ No3_列表的顺序.py
@@ -0,0 +1,14 @@
+#关于旅游景点地区的列表排序代码
+places =["beijing","shanghai","shanxi","guangzhou","shenzhen"]
+print("原始列表：",places)
+#使得旅游景点可以按照字母顺序排序
+print(sorted(places))
+print(places)
+print(sorted(places,reverse=True))
+print(places)
+places.reverse()
+print(places)
+places.sort
+print(places)
+places.sort(reverse=True)
+print(places)
